d2792ba2394370ba8e793beea2d63661f46c9eba
vitorlc <vitor_cirqueira1@hotmail.com>
2019-08-26 19:56:07 -0300

    add headless and w/h

--- getPic.js
+++ getPic.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 
 async function getPic() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://amazon.com.br');
+  await page.setViewport({width: 1000, height: 500})
   await page.screenshot({path: 'google1.png'});
 
   await browser.close();
